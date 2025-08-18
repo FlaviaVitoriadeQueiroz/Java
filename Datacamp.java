0f0334bf6e5f2fab47c32adbf244786d32258b59
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-08-18 16:38:34 -0300

    Add files via upload

---
+++ Datacamp.java
@@ -0,0 +1,12 @@
+public class Datacamp{ //declarando uma classe
+    public static void main(String [] args) {
+        System.out.println("Java");
+        System.out.println(18+9); //println tambem funciona como calculadora 
+        String name = "Queiroz"; /* o nome da variavel nao pode ter espaco, nem com _, usasse a  sintaxe Camel Case;
+                                    ou ser todo em letra maiuscula, ou comecar com letra maiuscula;
+                                    ou comecar com um numero*/
+        int age = 19;
+        System.out.println("My name is " + name + ", I am " + age + " years old");
+
+    }
+}
