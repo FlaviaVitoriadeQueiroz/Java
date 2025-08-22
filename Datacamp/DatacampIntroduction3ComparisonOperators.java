6198374e9288859b5dd787a17ac6793b7026ff3f
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-08-22 14:12:34 -0300

    Add files via upload

---
+++ Datacamp/DatacampIntroduction3ComparisonOperators.java
@@ -0,0 +1,18 @@
+//Comparison operators
+class DatacampIntroduction3ComparisonOperators {
+    public static void main(String [] args){
+        System.out.println(6>9);
+
+        int a = 26;
+        int v = 8;
+        System.out.println(a > v);
+
+        boolean m = 8 > 9;
+        System.out.println(m);
+
+        int minimo = 25;
+        int total = 26;
+        boolean freteGratis = total >= minimo;
+        System.out.println(freteGratis);
+    }
+}
