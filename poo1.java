370496f22175c92c98a07127b4bf021f55118e1d
Flávia Vitória de Queiroz <flaviavitoriaqueiroz@gmail.com>
2025-11-17 16:06:34 -0300

    Add files via upload

---
+++ poo1.java
@@ -0,0 +1,18 @@
+/*Um paradigma de programação é um estilo ou abordagem para resolver problemas com código, moldando como os desenvolvedores estruturam e organizam seu software
+Exemplos incluem a programação orientada a objetos (POO), que modela o mundo real com objetos.
+
+Organiza o código em torno de "objetos", que combinam dados (atributos) e comportamentos (métodos). Isso ajuda a modelar o mundo real,
+onde objetos como um "carro" podem ter atributos como "cor" e métodos como "andar".
+
+mais segura,
+reutilizavél,
+para problemas complexos,
+flexibilidade e abstração mais importantes,
+abordagem bottom-up
+
+pode ser complexo em problemas mais simples
+modelagem complexa (n de classes e relações entre elas)
+*/
+
+/*Java é uma linguagem híbrida, compilada e interpretada ao mesmo tempo */
+
